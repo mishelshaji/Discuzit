9752b97007ac75add868b1bacf4e708c36b243f8
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2020-10-21 20:22:36 +0530

    Configure PasswordSigninAsync to use email

--- Discuzit/Controllers/AccountController.cs
+++ Discuzit/Controllers/AccountController.cs
@@ -77,7 +77,8 @@ namespace Discuzit.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var user = await UserManager.FindByEmailAsync(model.Email);
+            var result = await SignInManager.PasswordSignInAsync(user.UserName, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
