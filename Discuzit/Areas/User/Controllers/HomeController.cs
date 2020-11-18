292a8950da6da594282229e88e3d8ecae5291574
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2020-11-18 21:19:13 +0530

    List questions in User area

--- Discuzit/Areas/User/Controllers/HomeController.cs
+++ Discuzit/Areas/User/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Discuzit.Models;
 using Discuzit.Shared;
 
 namespace Discuzit.Areas.User.Controllers
@@ -10,10 +12,18 @@ namespace Discuzit.Areas.User.Controllers
     [Authorize(Roles = UserRoles.IsUser)]
     public class HomeController : Controller
     {
+        private ApplicationDbContext _db = new ApplicationDbContext();
         // GET: User/Home
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Questions()
+        {
+            var userId = _db.Users.FirstOrDefault(x => x.UserName == System.Web.HttpContext.Current.User.Identity.Name)?.Id;
+            var questions = _db.Questions.Where(m => m.CreatedBy == userId).Include(m=>m.Category);
+            return View(questions);
         }
     }
 }
